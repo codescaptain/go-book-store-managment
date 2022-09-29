a1ff1ece67133419ca19cd65bda6322c8b1607ac
codescaptain <codescaptain@gmail.com>
2022-09-29 14:28:44 +0300

    update db name and routes

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,13 +1,16 @@
 package config
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 var (
 	db *gorm.DB
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:root/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:@tcp(localhost:3306)/go?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
